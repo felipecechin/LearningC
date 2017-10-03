b0c6dbb67bb9c3467db23c3cf8dbbce5d95f104e
Felipe Cechin Mello <ficechin@hotmail.com>
2017-10-03 19:37:52 -0300

    Exercício a ser feito!

---
+++ aula0310/exercicio3.c
@@ -0,0 +1,6 @@
+//O número 3025 é interessante: 30 + 25 = 55 e 55^2 = 3025. Faça uma função que recebe um número e e retorna um bool 
+//que diz se esse número tem essa mesma interessância. Faça um programa que usa essa função e um comando for para 
+//imprimir todos os números de 4 dígitos que tenham essa mesma característica. 
+
+
+//Ler um vetor de números e dizer se o vetor está na ordem ou não (do menor para o maior)
