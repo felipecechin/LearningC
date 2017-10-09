d8ff08524789caabec4c7021f7204cd2a553dbc9
Felipe Cechin Mello <ficechin@hotmail.com>
2017-10-08 23:13:06 -0300

    Exercício que verifica se um número é quadrado perfeito

--- aula0510/quadradoPerfeito.c
+++ aula0510/quadradoPerfeito.c
@@ -1,5 +1,24 @@
+//Verificar se um numero eh quadrado perfeito atraves da decomposicao por fatores primos
 #include <stdio.h>
+#include <stdbool.h>
 #define TAM 5
+
+bool numeroPrimo(int numero) 
+{
+  int i;
+  int cont = 0;
+  for (i=1;i<numero;i++) {
+    if (numero%i == 0) {
+      cont++;
+	}
+  }
+  if (cont>1) {
+    return false;
+  } else {
+  	return true;
+  }
+}
+
 main()
 {
   int numero,i;
@@ -7,14 +26,41 @@ main()
   scanf("%d",&numero);
   
   while(numero>0) {
-    
-    for (i=1;i<numero;i++) {
-      if (numero%i) {
-      	cont++;
+	
+	if (numeroPrimo(numero)) {
+	  printf("O numero %d nao eh quadrado perfeito\n",numero);
+    } else {
+      bool quadPerfeito = true;
+	  int numerador = numero;
+	  int divisao;
+	  for (i=2;i<numero;i++) {
+	  	int contPrimo = 0;
+	    if (numeroPrimo(i)) {
+	      for(;;) {
+	      	if (numerador%i!=0) {
+	      	  break;	
+			}
+			contPrimo++;
+	        divisao = numerador/i;
+			numerador = divisao;
+			if (numerador == 1) {
+			  break;
+			}	
+		  }   
+		  if (contPrimo%2 !=0) {
+		    quadPerfeito = false;  
+		    break;
+		  }
+	      if (numerador == 1) {
+	        break;	
+		  }         
+	    }
 	  }
-	}
-    if (cont>1) {
-      quadPerfeito = false;
+	  if (quadPerfeito) {
+	  	printf("O numero %d eh quadrado perfeito\n",numero);
+	  } else {
+	  	printf("O numero %d nao eh quadrado perfeito\n",numero);
+	  }
 	}
 	
 	printf("Digite um numero: ");
