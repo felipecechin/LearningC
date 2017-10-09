13e16581a33465ec333316b29cea9a0dde916494
Felipe Cechin Mello <ficechin@hotmail.com>
2017-10-08 22:01:11 -0300

    Ordenação de vetor feito com o método bolha

--- aula0510/ordenacaoVetor.c
+++ aula0510/ordenacaoVetor.c
@@ -1,5 +1,6 @@
 //Ordenando um vetor do menor para o maior
 #include <stdio.h>
+#include <stdbool.h>
 #define TAM 5
 main()
 {
@@ -10,15 +11,29 @@ main()
     scanf("%d",&vetor[i]);
   }
   
-  for (i=0;i<(TAM-1);i++) {
-  	for (j=i+1;j<TAM;j++) {
-  	  printf("i-%d .... j-%d\n",i,j);
-  	  if (vetor[i] > vetor[j]) {
-  	    aux = vetor[i];
-		vetor[i] = vetor[j];
-		vetor[j] = aux;  
-      }
-    }
+//  for (i=0;i<(TAM-1);i++) {
+//  	for (j=i+1;j<TAM;j++) {
+//  	  printf("i-%d .... j-%d\n",i,j);
+//  	  if (vetor[i] > vetor[j]) {
+//  	    aux = vetor[i];
+//		vetor[i] = vetor[j];
+//		vetor[j] = aux;  
+//      }
+//    }
+//  }
+
+  bool acabou = true;
+
+  while (acabou) {
+    acabou = false;
+    for (i=0;i<TAM-1;i++) {
+      if (vetor[i] > vetor[i+1]) {
+      	aux = vetor[i];
+      	vetor[i] = vetor[i+1];
+      	vetor[i+1] = aux;
+      	acabou = true;
+	  }
+	}
   }
   
   for (i=0;i<TAM;i++) {
